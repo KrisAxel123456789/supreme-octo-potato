18a6163352fd88a7490529ad124d3c4168f464b4
KrisAxel123456789 <125164226+KrisAxel123456789@users.noreply.github.com>
2023-02-12 00:04:53 +0000

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,39 @@
+s = Instance.new("Sound")
+ 
+s.Name = "Sound"
+s.SoundId = "https://web.roblox.com/library/2672209057/Rick-Roll"
+s.Volume = 25
+s.Looped = false
+s.archivable = true
+s.PlayOnRemove = true
+s.Parent = script
+wait(1)
+s:play()
+wait(19)
+h = Instance.new("Hint")
+h.Parent = script
+h.Text = "We're no strangers to love"
+wait(4.3)
+h.Text = "You know the rules and so do I!"
+wait(4.3)
+h.Text = "A full commitment's what I'm thinking of"
+wait(4.3)
+h.Text = "You wouldnt get this from any other guy!"
+wait(4)
+h.Text = "I just wanna tell you how I'm feeling"
+wait(4)
+h.Text = "Gotta make you understand"
+wait(3)
+h.Text = "Never gonna give you up"
+wait(2.5)
+h.Text = "Never gonna let you down"
+wait(2.5)
+h.Text = "Never gonna run around and desert you"
+wait(4)
+h.Text = "Never gonna make you cry"
+wait(2.7)
+h.Text = "Never gonna say goodbye"
+wait(3)
+h.Text = "Never gonna tell a lie and hurt you"
+wait(4)
+script:remove()
